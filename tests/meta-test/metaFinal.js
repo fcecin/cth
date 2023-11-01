4550322b3ada2fe3a29a98aac831bcce4813702e
fcecin <fcecin@gmail.com>
2023-10-31 22:57:01 -0300

    finished meta-test

--- tests/meta-test/metaFinal.js
+++ tests/meta-test/metaFinal.js
@@ -1,3 +1,23 @@
+// --------------------------------------------------------------------------------------------
+// Test meta game end
+// --------------------------------------------------------------------------------------------
 
-- massive time has passed
-- check auctions have all ended and you can't do anything now
+{
+    // far more days than necessary to end the metagame have elapsed
+    clock.clockaddsec( 86400 * 50 );
+
+    fixtureLog( "Meta update all the way to the end of the metagame:\n" + meta.update(Number.MAX_SAFE_INTEGER) );
+
+    let success = true;
+    try {
+        meta._("metaplayer11").dostake("metaplayer11", "1.000 TCN", 1);
+        success = false;
+    } catch (error) {
+        if (error.message.includes('meta game has ended')) {
+            fixtureLog("Correctly got an error from trying to do something after the meta game has ended");
+        } else {
+            fixtureWarningLog("Correctly got an error from trying to do something after the meta game has ended, but it's not the expected error message: " + error);
+        }
+    }
+    assert(`${success}`, `successfully failed to do something after the meta game has ended`);
+}
