9a3cc680a511a7b58ab46eaad3c89fb79f0d8794
fcecin <fcecin@gmail.com>
2023-11-11 17:00:34 -0300

    variant example for hgm v2

--- tests/gvm/gvm_VARIANT_WORKING.cpp
+++ tests/gvm/gvm_VARIANT_WORKING.cpp
@@ -2,42 +2,9 @@
 #include <eosio/print.hpp>
 using namespace eosio;
 
+//----------------
+
 #include <variant>
-
-// ----------------------------------------------------------------------------
-
-/*
-sample output (cth gvm):
-
-TEST: fixtureInit(): initializing test...
-TEST: init(): setting up test environment...
-doh_hotstart_start: successfully loaded and parsed 173 DoH constants from readonly contract
-doh_hotstart_start: successfully started at P2P port 10002 (HTTP port: 20002)
-TEST: init(): parsed 207 DoH error codes from '/home/fcecin/cth/local/test/doh-contracts/doh-common-code/error_codes.hpp'.
-TEST: init(): OK
-TEST: fixtureInit(): initialization OK.
-test variants
-#    eosio.null <= eosio.null::nonce            "424563dde5090600"
-#           gvm <= gvm::testvar                 ""
->> int 42
->> double 3.140000000000000e+00
->> std::string Hello
->> AStruct 500
->> BStruct true
-test variants done
-TEST: fixtureFinish(): finishing test...
-TEST: finish(): starting cleanup...
-TEST: finish(): cleanup OK.
-
-Fixture testing summary:
-
-
-TEST: fixtureFinish(): completed all (0) tests successfully.
-TEST: fixtureFinish(): process.exit(0)
-
-*/
-
-// ----------------------------------------------------------------------------
 
 struct AStruct {
    int i;
@@ -50,10 +17,6 @@ struct BStruct {
 class MyClass {
 public:
 
-};
-
-CONTRACT gvm : public contract {
-private:
    // Called when you forgot to write the method for a variant
    template <typename T>
    void othermethod(T& obj) {
@@ -85,30 +48,34 @@ private:
       auto visitor = [this](auto&& arg) { this->othermethod(arg); };
       std::visit(visitor, var);
    }
+};
 
+CONTRACT gvm : public contract {
 public:
     using contract::contract;
 
     [[eosio::action]]
     void testvar() {
+       MyClass obj;
+       
        typedef
           std::variant<int, double, std::string, AStruct, BStruct>
           VT;
        
        VT var1 = 42;
-       processVariant(var1);
+       obj.processVariant(var1);
        
        VT var2 = 3.14;
-       processVariant(var2);
+       obj.processVariant(var2);
        
        VT var3 = std::string("Hello");
-       processVariant(var3);
+       obj.processVariant(var3);
        
        VT var4 = AStruct{500};
-       processVariant(var4);
+       obj.processVariant(var4);
        
        VT var5 = BStruct{true};
-       processVariant(var5);
+       obj.processVariant(var5);
     }
 
     gvm(name receiver, name code, datastream<const char*> ds):
